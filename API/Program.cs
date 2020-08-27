6d96206b99a32c2e5a88f0777094930dbd76e4a5
Jason Ibisi <j.ibisi@nestoilgroup.com>
2020-08-27 11:56:34 +0100

    Added MediatR to Application project
    API - Get activities using MediatR in Activities Controller

--- API/Program.cs
+++ API/Program.cs
@@ -26,6 +26,7 @@ namespace API
               {
                 var context = services.GetRequiredService<DataContext>();
                 context.Database.Migrate();
+                Seed.SeedDatabase(context);
               }
               catch (Exception ex)
               {
