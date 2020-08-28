7791f318f574d2394f782021c05ee6d445aaf5e5
Jason IBISI <jasonibisi@gmail.com>
2020-08-28 15:46:48 +0100

    Client App: Added NavBar and ActivityDashboard components

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css'
-import App from './App';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
