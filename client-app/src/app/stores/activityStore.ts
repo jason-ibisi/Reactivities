b8626e28f025fc136edd081b4441beb37a052575
Jason IBISI <jasonibisi@gmail.com>
2020-08-30 11:26:39 +0100

    Client App: Setup MobX
    Refactored activity list and select activities to use store

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,33 @@
+import { IActivity } from './../models/activity';
+import { observable, action } from 'mobx';
+import { createContext } from 'react';
+import agent from '../api/agent';
+
+class ActivityStore {
+  @observable activities: IActivity[] = [];
+  @observable selectedActivity: IActivity | null | undefined;
+  @observable loadingInitial = false;
+  @observable editMode = false;
+
+  @action loadActivities = async () => {
+    this.loadingInitial = true;
+    try {
+      const activities = await agent.Activities.list(); 
+      activities.forEach((activity) => {
+        activity.date = activity.date.split('.')[0];
+        this.activities.push(activity);
+      });
+      this.loadingInitial = false; 
+    } catch (error) {
+      console.log(error);
+      this.loadingInitial = false; 
+    }
+  }
+
+  @action selectActivity = (id: string) => {
+     this.selectedActivity = this.activities.find(a => a.id === id);
+     this.editMode = false;
+  }
+}
+
+export default createContext(new ActivityStore());
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,16 @@
-import React, { SyntheticEvent } from 'react';
+import React, { SyntheticEvent, useContext } from 'react';
 import { Grid, GridColumn } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../../form/ActivityForm';
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
 
 interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
-  selectedActivity: IActivity | null;
   setSelectedActivity: (activity: IActivity | null) => void;
-  editMode: boolean;
   setEditMode: (editMode: boolean) => void;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
@@ -22,9 +22,7 @@ interface IProps {
 const ActivityDashboard: React.FC<IProps> = ({
   activities,
   selectActivity,
-  selectedActivity,
   setSelectedActivity,
-  editMode,
   setEditMode,
   createActivity,
   editActivity,
@@ -32,12 +30,13 @@ const ActivityDashboard: React.FC<IProps> = ({
   submitting,
   target,
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const { editMode, selectedActivity } = activityStore;
+
   return (
     <Grid>
       <GridColumn width={10}>
         <ActivityList
-          activities={activities}
-          selectActivity={selectActivity}
           deleteActivity={deleteActivity}
           submitting={submitting}
           target={target}
@@ -46,7 +45,6 @@ const ActivityDashboard: React.FC<IProps> = ({
       <GridColumn width={6}>
         {selectedActivity && !editMode && (
           <ActivityDetails
-            activity={selectedActivity}
             setSelectedActivity={setSelectedActivity}
             setEditMode={setEditMode}
           />
@@ -55,7 +53,7 @@ const ActivityDashboard: React.FC<IProps> = ({
           <ActivityForm
             key={(selectedActivity && selectedActivity.id) || 0}
             setEditMode={setEditMode}
-            activity={selectedActivity}
+            activity={selectedActivity!}
             createActivity={createActivity}
             editActivity={editActivity}
             submitting={submitting}
@@ -66,4 +64,4 @@ const ActivityDashboard: React.FC<IProps> = ({
   );
 };
 
-export default ActivityDashboard;
+export default observer(ActivityDashboard);
