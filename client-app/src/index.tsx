cf72f561722bff6b932705f9b68836e34fbdcb2e
Jason IBISI <jasonibisi@gmail.com>
2020-09-06 16:30:11 +0100

    Client App: Used Axios interceptors to catch 5xx and Network unreacheable errors

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
