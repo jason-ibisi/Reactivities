7de18ffb30e94d855edb46fa17a98a5147bcc4f6
Jason IBISI <jasonibisi@gmail.com>
2020-09-11 22:31:40 +0100

    Application: Return the JWT Token on successful login

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Errors;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
@@ -30,8 +31,11 @@ namespace Application.User
     {
       private readonly UserManager<AppUser> _userManager;
       private readonly SignInManager<AppUser> _signInManager;
-      public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+      private readonly IJwtGenerator _jwtGenerator;
+      public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+      IJwtGenerator jwtGenerator)
       {
+        _jwtGenerator = jwtGenerator;
         _signInManager = signInManager;
         _userManager = userManager;
       }
@@ -45,12 +49,13 @@ namespace Application.User
 
         var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
 
-        if (result.Succeeded) {
+        if (result.Succeeded)
+        {
           // TODO: Generate token 
-          return new User 
+          return new User
           {
             DisplayName = user.DisplayName,
-            Token = "Token Placeholder",
+            Token = _jwtGenerator.CreateToken(user),
             Username = user.UserName,
             Image = null
           };
