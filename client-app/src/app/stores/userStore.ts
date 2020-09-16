ab9ea1719facc1fdad527f241c3a8ee68b95d6ac
Jason IBISI <jasonibisi@gmail.com>
2020-09-16 23:44:05 +0100

    Client App: Created user interfaces and axios methos
    Created MobX user store

---
+++ client-app/src/app/stores/userStore.ts
@@ -0,0 +1,19 @@
+import { IUserFormValues } from './../models/user';
+import { observable, computed, action } from "mobx";
+import { IUser } from "../models/user";
+import agent from '../api/agent';
+
+export default class UserStore {
+  @observable user: IUser | null = null;
+
+  @computed get isLoggedIn() {return !!this.user}
+
+  @action login = async (values: IUserFormValues) => {
+    try {
+      const user = await agent.User.login(values);
+      this.user = user;
+    }catch (error) {
+      console.log(error);
+    }
+  }
+}
